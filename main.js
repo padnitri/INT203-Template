bbdae73cbf9d06ce990946de8d607ab5b1f25ff4
padnitri <57793978+padnitri@users.noreply.github.com>
2021-02-14 23:57:18 +0700

    added insided items

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-    const app = {
-        data() {
-            return {
-                msg: 'Hello, Vue3'
-            }
+const app = {
+    data() {
+        return {
+            tasks: [{ name: 'Lobster', img: './images/001.jpg', isFav: false },
+            { name: 'Mussel', img: './images/002.jpg', isFav: false },
+            { name: 'Crab', img: './images/003.jpg', isFav: false }
+            ],
+            favCount: 0,
         }
+    },
+    methods:
 
+    {
+        toggleDone(index) {
+            this.tasks[index].done = !this.tasks[index].done
+        },
+        updateFav(index) {
+
+            this.tasks[index].isFav = !this.tasks[index].isFav
+            if (this.tasks[index].isFav) {
+                this.favCount += 1
+            } else {
+                this.favCount -= 1
+            }
+
+        }
+    },
+    computed: {
+        calculate() {
+            return this.tasks.length
+        }
     }
-    Vue.createApp(app).mount('#app')
+}
+Vue.createApp(app).mount('#app')
